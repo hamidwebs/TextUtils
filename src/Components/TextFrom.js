f078a7bc3aaf6ad132e26ddee71cbad681f10c4d
Mughalsahab <144914285+Mughalsahab@users.noreply.github.com>
2024-04-07 03:36:09 +0500

    Bye

--- src/Components/TextFrom.js
+++ src/Components/TextFrom.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 export default function EnterText(props) {
+    // Functions
     const upperCasing = () => {
         updatedText(oldText.toUpperCase())
     }
@@ -16,6 +17,11 @@ export default function EnterText(props) {
                 }
             }
         }
+        if (oldText.length === 0) {
+            wordNumbersUpdating(0);
+        } else {
+            wordNumbersUpdating(oldText.split(" ").length);
+        }
         spaceNumbersUpdating(spaces);
     }
     const lowerCasing = () => {
@@ -23,11 +29,21 @@ export default function EnterText(props) {
     }
     const clearing = () => {
         updatedText("");
+        wordNumbersUpdating(0);
+    }
+    const handleBoldClick = () => {
+        setIsBold(true);
+    };
+    const handleItalicClick = () => {
+        setItalic(true);
     }
     // States
+    const [isItalic, setItalic] = useState(false);
+    const [isBold, setIsBold] = useState(false);
     const [oldText, updatedText] = useState('');
+    const [wordNumber, wordNumbersUpdating] = useState(0);
     const [spaceNumbers, spaceNumbersUpdating] = useState(0);
-
+    // Return Module
     return (
         <>
             <div className='container'>
@@ -37,9 +53,13 @@ export default function EnterText(props) {
                     <textarea className="form-control" onKeyUp={writing} onChange={onChanging} value={oldText} id="input-box" rows="10" />
                 </div>
                 <h5>Functions : </h5>
-                <button className="btn btn-outline-success m-3" onClick={lowerCasing}>To Lower Case</button>
-                <button className="btn btn-outline-info m-3" onClick={upperCasing}>To Upper Case</button>
-                <button className="btn btn-outline-danger m-3" onClick={clearing}>Clear Text</button>
+                <div className='text-center'>
+                    <button className="btn btn-outline-success m-3 px-5" onClick={lowerCasing}>To lower case</button>
+                    <button className="btn btn-outline-info m-3 px-5" onClick={upperCasing}>To UPPER CASE</button>
+                    <button className="btn btn-outline-primary m-3 px-5" onClick={handleBoldClick}>To <b>Bold</b></button>
+                    <button className="btn btn-outline-primary m-3 px-5" onClick={handleItalicClick}>To <i>Italic</i></button>
+                    <button className="btn btn-outline-danger m-3 px-5" onClick={clearing}>Clear Text</button>
+                </div>
             </div>
             <div className="container m-3">
                 <h2>Your Text Summary : </h2>
@@ -49,7 +69,7 @@ export default function EnterText(props) {
                             <div className="fw-bold">Words</div>
                             i.e., TextUtils a word.
                         </div>
-                        <span className="badge text-bg-primary rounded-pill">{oldText.split(" ").length}</span>
+                        <span className="badge text-bg-primary rounded-pill">{wordNumber}</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between align-items-start">
                         <div className="ms-2 me-auto">
@@ -68,15 +88,15 @@ export default function EnterText(props) {
                     <li className="list-group-item d-flex justify-content-between align-items-start">
                         <div className="ms-2 me-auto">
                             <div className="fw-bold">Time to Read</div>
-                            i.e., It takes about 0.008 minute to read one word.
+                            i.e., It takes about 0.48 Second to read one word.
                         </div>
-                        <span className="badge text-bg-primary rounded-pill">{0.008 * oldText.split(" ").length} Minutes</span>
+                        <span className="badge text-bg-primary rounded-pill">{oldText.length === 0 ? 0 : (0.48 * oldText.split(" ").length)} Seconds</span>
                     </li>
                 </ol>
             </div>
             <div className="container m-3">
                 <h2>Preview : </h2>
-                <p className="lead">
+                <p className="lead" style={{ fontWeight: isBold ? 'bold' : 'normal', fontStyle: isItalic ? 'italic' : 'normal' }}>
                     &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;{oldText}
                 </p>
             </div>
