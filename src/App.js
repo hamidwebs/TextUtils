7de7f1eee9ac034fa04678ee418380c62d87b245
Mughalsahab <144914285+Mughalsahab@users.noreply.github.com>
2024-04-13 19:09:00 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -40,14 +40,14 @@ function App() {
         <Navbar title="TextUtils" aboutText="About TextUtils" aboutLink="/" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} mode={mode} />
         <Switch>
+          <Route exact path="/">
+            <div className="container my-4">
+              <TextFrom heading="Enter Text to Analyze" mode={mode} toggleMode={toggleMode} showAlert={showAlert} />
+            </div>
+          </Route>
           <Route exact path="/about">
             <div className="container my-4">
               <About mode={mode} />
-            </div>
-          </Route>
-          <Route path="/TextUtils">
-            <div className="container my-4">
-              <TextFrom heading="Enter Text to Analyze" mode={mode} toggleMode={toggleMode} showAlert={showAlert} />
             </div>
           </Route>
         </Switch>
